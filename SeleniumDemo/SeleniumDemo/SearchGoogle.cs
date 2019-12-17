98691cb699adb1d215e32fcf65deaf3081f9504d
nguyentrucxinh <nguyentrucxjnh@gmail.com>
2019-12-17 14:59:32 +0700

    :init: Add selenium sample

---
+++ SeleniumDemo/SeleniumDemo/SearchGoogle.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SeleniumDemo
+{
+    [TestClass]
+    public class SearchGoogle
+    {
+        [TestMethod]
+        public void SearchForCheese()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
+            {
+                driver.Navigate().GoToUrl("http://www.google.com/");
+
+                IWebElement query = driver.FindElement(By.Name("q"));
+
+                query.SendKeys("Cheese");
+
+                query.Submit();
+
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+
+                Assert.AreEqual(driver.Title, "Cheese - Google Search");
+            }
+        }
+    }
+}
