bf04a940b049eac0653ebce3124e09110ab71809
nguyentrucxinh <nguyentrucxjnh@gmail.com>
2019-12-22 22:00:43 +0700

    :sparkles: Update API Versioning with Swagger

--- health-check/src/EfCosmos.Services.Api/Controllers/SampleApiVersionController.cs
+++ health-check/src/EfCosmos.Services.Api/Controllers/SampleApiVersionController.cs
@@ -4,7 +4,24 @@ using System.Collections.Generic;
 namespace EfCosmos.Services.Api.Controllers
 {
     [ApiController]
-    [ApiVersion("1.0", Deprecated = true)]
+    [ApiVersion("0.0", Deprecated = true)]
+    [Route("api/Values")]
+    public class ValuesV0Controller : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[]
+            {
+                "Value1 from Version 0",
+                "value2 from Version 0"
+            };
+        }
+    }
+
+    [ApiController]
+    [ApiVersion("1.0")]
     [Route("api/Values")]
     public class ValuesV1Controller : Controller
     {
--- health-check/src/EfCosmos.Services.Api/Startup.cs
+++ health-check/src/EfCosmos.Services.Api/Startup.cs
@@ -4,11 +4,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace EfCosmos.Services.Api
 {
@@ -28,17 +30,38 @@ namespace EfCosmos.Services.Api
             services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(connectionString));
             services.AddControllers();
 
-            services.AddApiVersioning(o => {
-                o.ReportApiVersions = true;
-                o.AssumeDefaultVersionWhenUnspecified = true;
-                o.DefaultApiVersion = new ApiVersion(1, 0);
-                o.ApiVersionReader = ApiVersionReader.Combine(
-                    new QueryStringApiVersionReader(),
-                    new HeaderApiVersionReader()
-                    {
-                        HeaderNames = { "api-version" }
-                    });
-            });
+            services.AddApiVersioning(
+                options =>
+                {
+                    // reporting api versions will return the headers "api-supported-versions" and "api-deprecated-versions"
+                    options.ReportApiVersions = true;
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.DefaultApiVersion = new ApiVersion(1, 0);
+                    //options.ApiVersionReader = ApiVersionReader.Combine(
+                    //    new QueryStringApiVersionReader(),
+                    //    new HeaderApiVersionReader()
+                    //    {
+                    //        HeaderNames = { "api-version" }
+                    //    });
+                });
+            services.AddVersionedApiExplorer(
+                options =>
+                {
+                    // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                    // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
+                });
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen(
+                options =>
+                {
+                    // add a custom operation filter which sets default values
+                    options.OperationFilter<SwaggerDefaultValues>();
+                });
 
             // Health check
             services.AddHealthChecks()
@@ -51,7 +74,7 @@ namespace EfCosmos.Services.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -75,6 +98,17 @@ namespace EfCosmos.Services.Api
 
                 endpoints.MapHealthChecksUI();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    // build a swagger endpoint for each discovered API version
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                    }
+                });
         }
     }
 }
