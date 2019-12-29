414b1b80ae49d69f9258f09a3233c7f2f58726e1
nguyentrucxinh <nguyentrucxjnh@gmail.com>
2019-12-29 19:52:54 +0700

    :fire: Add project

---
+++ global-error-handling/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace global_error_handling.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get() =>
+            GetWeather();
+
+        [HttpGet("{city}")]
+        public WeatherForecast Get(string city)
+        {
+            if (!string.Equals(city?.TrimEnd(), "Redmond", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException(
+                    $"We don't offer a weather forecast for {city}.", nameof(city));
+            }
+            
+            return GetWeather().First();
+        }
+
+        private IEnumerable<WeatherForecast> GetWeather()
+        {
+            string[] summaries = new[]
+            {
+                "Rainy", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+            var rng = new Random();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = summaries[rng.Next(summaries.Length)]
+            }).ToArray();
+        }
+    }
+}
